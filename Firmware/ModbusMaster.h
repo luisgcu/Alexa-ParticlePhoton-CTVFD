2e66c15a414c38d047b53aa1f8f2791072b0e276
Guerrero <Angel.Guerrero@mail.nidec.com>
2017-09-28 11:23:14 -0400

    RE arrange Folders

--- Firmware/ModbusMaster.h
+++ Firmware/ModbusMaster.h
@@ -10,29 +10,29 @@ Arduino library for communicating with Modbus slaves over RS232/485 (via RTU pro
 /*
   ModbusMaster.h - Arduino library for communicating with Modbus slaves
   over RS232/485 (via RTU protocol).
-
+  
   This file is part of ModbusMaster.
-
+  
   ModbusMaster is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-
+  
   ModbusMaster is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-
+  
   You should have received a copy of the GNU General Public License
   along with ModbusMaster.  If not, see <http://www.gnu.org/licenses/>.
-
+  
   Written by Doc Walker (Rx)
   Copyright Â© 2009-2013 Doc Walker <4-20ma at wvfans dot net>
   Adapted for Spark Core by Paul Kourany, March 14, 2014
   Further modifications and port to Particle Photon by Anurag Chugh, July 5, 2016 <lithiumhead at gmail dot com>
 */
 
-
+  
 #ifndef ModbusMaster_h
 #define ModbusMaster_h
 
@@ -79,7 +79,7 @@ Macro to generate 32-bit integer from (2) 16-bit words.
 
 /* _____CLASS DEFINITIONS____________________________________________________ */
 /**
-Arduino class library for communicating with Modbus slaves over
+Arduino class library for communicating with Modbus slaves over 
 RS232/485 (via RTU protocol).
 */
 class ModbusMaster {
@@ -87,67 +87,67 @@ public:
 	ModbusMaster();
 	ModbusMaster(uint8_t);
 	ModbusMaster(uint8_t, uint8_t);
-
+	
 	void begin();
 	void begin(uint16_t);
 	void idle(void (*)());
-
+	
 	// Modbus exception codes
 	/**
 	Modbus protocol illegal function exception.
-
+	
 	The function code received in the query is not an allowable action for
 	the server (or slave). This may be because the function code is only
 	applicable to newer devices, and was not implemented in the unit
 	selected. It could also indicate that the server (or slave) is in the
 	wrong state to process a request of this type, for example because it is
 	unconfigured and is being asked to return register values.
-
+	
 	@ingroup constant
 	*/
 	static const uint8_t ku8MBIllegalFunction			= 0x01;
 
 	/**
 	Modbus protocol illegal data address exception.
-
-	The data address received in the query is not an allowable address for
-	the server (or slave). More specifically, the combination of reference
-	number and transfer length is invalid. For a controller with 100
-	registers, the ADU addresses the first register as 0, and the last one
-	as 99. If a request is submitted with a starting register address of 96
-	and a quantity of registers of 4, then this request will successfully
-	operate (address-wise at least) on registers 96, 97, 98, 99. If a
-	request is submitted with a starting register address of 96 and a
-	quantity of registers of 5, then this request will fail with Exception
-	Code 0x02 "Illegal Data Address" since it attempts to operate on
-	registers 96, 97, 98, 99 and 100, and there is no register with address
-	100.
-
+	
+	The data address received in the query is not an allowable address for 
+	the server (or slave). More specifically, the combination of reference 
+	number and transfer length is invalid. For a controller with 100 
+	registers, the ADU addresses the first register as 0, and the last one 
+	as 99. If a request is submitted with a starting register address of 96 
+	and a quantity of registers of 4, then this request will successfully 
+	operate (address-wise at least) on registers 96, 97, 98, 99. If a 
+	request is submitted with a starting register address of 96 and a 
+	quantity of registers of 5, then this request will fail with Exception 
+	Code 0x02 "Illegal Data Address" since it attempts to operate on 
+	registers 96, 97, 98, 99 and 100, and there is no register with address 
+	100. 
+	
 	@ingroup constant
 	*/
 	static const uint8_t ku8MBIllegalDataAddress		 = 0x02;
-
+	
 	/**
 	Modbus protocol illegal data value exception.
-
-	A value contained in the query data field is not an allowable value for
-	server (or slave). This indicates a fault in the structure of the
-	remainder of a complex request, such as that the implied length is
-	incorrect. It specifically does NOT mean that a data item submitted for
-	storage in a register has a value outside the expectation of the
-	application program, since the MODBUS protocol is unaware of the
+	
+	A value contained in the query data field is not an allowable value for 
+	server (or slave). This indicates a fault in the structure of the 
+	remainder of a complex request, such as that the implied length is 
+	incorrect. It specifically does NOT mean that a data item submitted for 
+	storage in a register has a value outside the expectation of the 
+	application program, since the MODBUS protocol is unaware of the 
 	significance of any particular value of any particular register.
-
+	
 	@ingroup constant
 	*/
 	static const uint8_t ku8MBIllegalDataValue		   = 0x03;
-
+	
 	/**
 	Modbus protocol slave device failure exception.
-
+	
 	An unrecoverable error occurred while the server (or slave) was
 	attempting to perform the requested action.
-
+	
 	@ingroup constant
 	*/
 	static const uint8_t ku8MBSlaveDeviceFailure		 = 0x04;
@@ -155,60 +155,60 @@ public:
 	// Class-defined success/exception codes
 	/**
 	ModbusMaster success.
-
+	
 	Modbus transaction was successful; the following checks were valid:
 	  - slave ID
 	  - function code
 	  - response code
 	  - data
 	  - CRC
-
+	  
 	@ingroup constant
 	*/
 	static const uint8_t ku8MBSuccess					= 0x00;
-
+	
 	/**
 	ModbusMaster invalid response slave ID exception.
-
+	
 	The slave ID in the response does not match that of the request.
-
+	
 	@ingroup constant
 	*/
 	static const uint8_t ku8MBInvalidSlaveID			 = 0xE0;
-
+	
 	/**
 	ModbusMaster invalid response function exception.
-
+	
 	The function code in the response does not match that of the request.
-
+	
 	@ingroup constant
 	*/
 	static const uint8_t ku8MBInvalidFunction			= 0xE1;
-
+	
 	/**
 	ModbusMaster response timed out exception.
-
-	The entire response was not received within the timeout period,
-	ModbusMaster::ku8MBResponseTimeout.
-
+	
+	The entire response was not received within the timeout period, 
+	ModbusMaster::ku8MBResponseTimeout. 
+	
 	@ingroup constant
 	*/
 	static const uint8_t ku8MBResponseTimedOut		   = 0xE2;
-
+	
 	/**
 	ModbusMaster invalid response CRC exception.
-
+	
 	The CRC in the response does not match the one calculated.
-
+	
 	@ingroup constant
 	*/
 	static const uint8_t ku8MBInvalidCRC				 = 0xE3;
-
+	
 	uint16_t getResponseBuffer(uint8_t);
 	void	 clearResponseBuffer();
 	uint8_t  setTransmitBuffer(uint8_t, uint16_t);
 	void	 clearTransmitBuffer();
-
+	
 	void beginTransmission(uint16_t);
 	uint8_t requestFrom(uint16_t, uint16_t);
 	void sendBit(bool);
@@ -217,12 +217,12 @@ public:
 	void send(uint32_t);
 	uint8_t available(void);
 	uint16_t receive(void);
-
+	
 	void enableDebug();
 	void disableDebug();
 	void enableTXpin(uint8_t);
 	void disableTXpin();
-
+	
 	uint8_t  readCoils(uint16_t, uint16_t);
 	uint8_t  readDiscreteInputs(uint16_t, uint16_t);
 	uint8_t  readHoldingRegisters(uint16_t, uint16_t);
@@ -236,12 +236,12 @@ public:
 	uint8_t  maskWriteRegister(uint16_t, uint16_t, uint16_t);
 	uint8_t  readWriteMultipleRegisters(uint16_t, uint16_t, uint16_t, uint16_t);
 	uint8_t  readWriteMultipleRegisters(uint16_t, uint16_t);
-
+	
 private:
 	uint8_t  _u8SerialPort;									  ///< serial port (0..3) initialized in constructor
 	uint8_t  _u8MBSlave;										 ///< Modbus slave (1..255) initialized in constructor
 	uint16_t _u16BaudRate;									   ///< baud rate (300..115200) initialized in begin()
-	static const uint8_t ku8MaxBufferSize				= 64;   ///< size of response/transmit buffers
+	static const uint8_t ku8MaxBufferSize				= 64;   ///< size of response/transmit buffers	
 	uint16_t _u16ReadAddress;									///< slave register from which to read
 	uint16_t _u16ReadQty;										///< quantity of words to read
 	uint16_t _u16ResponseBuffer[ku8MaxBufferSize];			   ///< buffer to store Modbus slave response; read via GetResponseBuffer()
@@ -254,7 +254,7 @@ private:
 	uint16_t* rxBuffer; // from Wire.h -- need to clean this up Rx
 	uint8_t _u8ResponseBufferIndex;
 	uint8_t _u8ResponseBufferLength;
-
+	
 	// Modbus function codes for bit access
 	static const uint8_t ku8MBReadCoils				  = 0x01; ///< Modbus function 0x01 Read Coils
 	static const uint8_t ku8MBReadDiscreteInputs		 = 0x02; ///< Modbus function 0x02 Read Discrete Inputs
@@ -268,13 +268,13 @@ private:
 	static const uint8_t ku8MBWriteMultipleRegisters	 = 0x10; ///< Modbus function 0x10 Write Multiple Registers
 	static const uint8_t ku8MBMaskWriteRegister		  = 0x16; ///< Modbus function 0x16 Mask Write Register
 	static const uint8_t ku8MBReadWriteMultipleRegisters = 0x17; ///< Modbus function 0x17 Read Write Multiple Registers
-
+	
 	// Modbus timeout [milliseconds]
-	static const uint8_t ku8MBResponseTimeout			= 1500;  ///< Modbus timeout [milliseconds] was 200
-
+	static const uint8_t ku8MBResponseTimeout			= 200;  ///< Modbus timeout [milliseconds]
+	
 	// master function that conducts Modbus transactions
 	uint8_t ModbusMasterTransaction(uint8_t u8MBFunction);
-
+	
 	// idle callback function; gets called during idle time between TX and RX
 	void (*_idle)();
 };
--- Firmware/MyModbusMaster4b.ino
+++ Firmware/MyModbusMaster4b.ino
@@ -1,20 +1,20 @@
-// This #include statement was automatically added by the Particle IDE.
-#include <MQTT.h>
+
 #include <ModbusMaster.h>
-#include "Adafruit_DHT.h"
-// Transmission delay between modbus calls
-#define XMITDELAY 300
-//#define POLLING_TIME 120000
-#define POLLING_TIME 60000
+#include <MQTT.h>
+#include <neopixel.h>
+#include <Adafruit_DHT.h>
+
+//#include "Particle.h"
+
+#define XMITDELAY 100
+//#define POLLING_TIME  Modbus master 120000
+#define POLLING_TIME 40000
 // 0 for 0-based, 1 for 1-based numbering
 #define BASED_NUMBERING 1
 #define DRIVEID  1
-#define reg_qty 11
-#define DHTPIN D1     // what pin we're connected to
-// Uncomment whatever type you're using!
+#define DHTPIN D1       // Humidity and temp sensor
 #define DHTTYPE DHT11		// DHT 11
-#include "Particle.h"
-#include "neopixel.h"
+
 // IMPORTANT: Set pixel COUNT, PIN and TYPE
 #define PIXEL_PIN D0
 #define PIXEL_COUNT 1
@@ -25,24 +25,31 @@ int green;
 int blue;
 int white;
 int clear_led;
+bool tripped = 0;
+uint16_t control_wrd         = 0;
 bool   DEBUGON               = 0;
 unsigned long previousMillis = 0;
 unsigned long previousMillis2= 0;
-// registros drive CT
-//uint16_t regs_open_loop[24]     ={200,401,402,404,417,419,420,426,501,502,503,504,505,704,705,734,735,1040,1811,1812,1813,1814,1815,1816};
-//uint16_t regs_cls_loop[24]      ={200,401,402,404,417,419,420,426,501,502,503,504,505,704,705,734,735,1040,1811,1812,1813,1814,1815,1816};
-uint16_t frozen_regs[17]        ={101,102,103,169,170,201,301,245,401,402,417,501,502,503,505,701,702};
+//char version* ="1.0";
+//char server1  ="iot4fun.ddns.net";
+//Modbus registers CT drive M200_M400 OPEN LOOP
+uint16_t frozen_regs[18]        ={101,102,103,169,170,201,301,401,402,417,501,502,503,505,701,702,1040,704};
+int16_t regs_dataf[18];
 uint16_t regs_CSK[24]           ={736,401,402,617,417,419,420,426,501,502,503,504,505,704,626,734,1020,1040,1811,1812,1813,1814,1815,1816};
 int16_t regs_data[24];
-//int regs_data[24];
+
 char vfd_data[450];
 char motor_data[450];
+char vfd_frzndata1[300];
+char vfd_frzndata2[300];
 char buf[350];
 
-//MQTT Settings
+//MQTT Settings Mosquitto runing  local on ubunto laptop.
 byte server[] = { 192,168,2,13 };//the IP of broker
 void callback(char* topic, byte* payload, unsigned int length);
-MQTT client(server, 1883, callback,700);
+//MQTT client(server, 1883, callback,700);
+//MQTT client(server, 1883, callback,700);
+MQTT client("iot4fun.ddns.net", 1883, callback,700);
 uint16_t qos2messageid = 0;
 
 // instantiate ModbusMaster object as slave ID 1
@@ -51,66 +58,132 @@ ModbusMaster node(Serial1,1);
 bool state = true;
 //int  data[6];   //was uint16_t
 int16_t data[2];
+int16_t dataf[2];
 int color_patern1 [9]; //all colors
 int color_patern2 [7]; //white
 int color_patern3 [7]; //green
 int color_patern4 [7]; //blue
 int color_patern5 [7]; //red
+//Dht funtion setup pin, an sensor type
 DHT dht(DHTPIN, DHTTYPE);
+// turn on/off drive funtion name
+int turn_On_Off_drive(String command);
+
+//int turnPump(String command);
+int  turn_On_Off_drive(String command)
+{
+	if (command == "ON")
+	{
+		  //led_patern1 (30);
+			 green_led();
+			 mb_wr_sr (641, 131);
+			 //control_wrd=131;
+			 //MBwriteSR();
+		  return 1;
+	}
+	else  if (command == "OFF")
+	{
+		  red_led();
+			mb_wr_sr (641, 128);
+			//control_wrd=128;
+			//MBwriteSR();
+		  return 1;
+	}
+	else
+	{
+		return -1;
+	}
+
+}
+
+
+//==========================================================
+
+
+bool mb_wr_sr (uint16_t regis , int16_t reg_val)
+{
+uint8_t result3;
+result3=node.writeSingleRegister(regis,reg_val);
+if (result3 == node.ku8MBSuccess)
+{
+   Particle.publish("Modbus Write OK");
+   return true;
+
+}
+else
+{
+   Particle.publish("Modbus Write FAIL");
+   return false;
+}
+node.clearResponseBuffer(); // if Timeout clearResponseBuffer and clearTransmitBuffer
+node.clearTransmitBuffer();
+//serialFlushInBuffer();
+result3=node.writeSingleRegister(regis,reg_val);
+}
+//==========================================================
+
+
+void serialFlushInBuffer()
+{
+  while(Serial1.read() >= 0);
+}
+/*
+//SYSTEM_MODE(AUTOMATIC);
+//============================================================================================
+//SYSTEM_MODE(SEMI_AUTOMATIC);
+// The semi-automatic mode will not attempt to connect the device to the Cloud automatically.
+// However once the device is connected to the Cloud (through some user intervention),
+//messages will be processed automatically, as in the automatic mode above.
+//============================================================================================
+
+//============================================================================================
+//SYSTEM_MODE(MANUAL);
+//he "manual" mode puts the device's connectivity completely in the user's control.
+//This means that the user is responsible for both establishing a connection to the
+//Particle Cloud and handling communications with the Cloud by calling Particle.process() on a regular basis.
+//============================================================================================
+
 void setup() {
+  // This is called immediately
+}
+
+void loop() {
+  if (buttonIsPressed()) {
+    Particle.connect();
+  } else {
+    doOfflineStuff();
+  }
+//or
+if (buttonIsPressed()) {
+    Particle.connect();
+  }
+  if (Particle.connected()) {
+    Particle.process();
+    doOtherStuff();
+  }
+
+}
+
+*/
+// Returns temperature VFD stack temperature
+int getStackTemperature(String args){
+    return regs_data[13];
+}
+// Return VFD DC bus voltage
+int getspeed(String args){
+    //return regs_data[12]; //DC BUS voltage
+		return regs_data[11]; //VFD SPEED
+}
+
+void setup() {
+  Particle.function("VFD-ONOFF", turn_On_Off_drive);
+  Particle.function("Stack-Temp", getStackTemperature);
+  Particle.function("Motor-Speed", getspeed);
+
+
 	delay(2000);
 		dht.begin();
-	  strip.begin();                         // Initialize strip
-	  strip.show();        // (R,  G,  B)    // Update all LEDs (= turn OFF, since none of them have been set yet!)
-	  red       = strip.Color(255, 0, 0);    // Red
-	  green     = strip.Color(0, 255, 0);    // Green
-	  blue      = strip.Color(0, 0, 255);    // Blue
-	  white     = strip.Color(243, 63, 193); // White  was (0,255,100) \\\violeta
-	  clear_led = strip.Color(0, 0, 0);      // clear
-		//patern1 test all colors
-		color_patern1 [0]=clear_led;
-		color_patern1 [1]=red;
-		color_patern1 [2]=clear_led;
-		color_patern1 [3]=green;
-		color_patern1 [4]=clear_led;
-		color_patern1 [5]=blue;
-		color_patern1 [6]=clear_led;
-		color_patern1 [7]=white;
-		color_patern1 [8]=clear_led;
-
-   //patern2 color white
-	  color_patern2 [0]=clear_led;
-		color_patern2 [1]= white;
-		color_patern2 [2]=clear_led;
-		color_patern2 [3]= white;
-		color_patern2 [4]=clear_led;
-		color_patern2 [5]= white;
-		color_patern2 [6]=clear_led;
-	 //patern3 color green
-	 color_patern3 [0]=clear_led;
-	 color_patern3 [1]= green;
-	 color_patern3 [2]=clear_led;
-	 color_patern3 [3]= green;
-	 color_patern3 [4]=clear_led;
-	 color_patern3 [5]= green;
-	 color_patern3 [6]=clear_led;
-	 //patern4 color blue
-	 color_patern4 [0]=clear_led;
-	 color_patern4 [1]= blue;
-	 color_patern4 [2]=clear_led;
-	 color_patern4 [3]= blue;
-	 color_patern4 [4]=clear_led;
-	 color_patern4 [5]= blue;
-	 color_patern4 [6]=clear_led;
-	 //patern5 color red
-	 color_patern5 [0]=clear_led;
-	 color_patern5 [1]= red;
-	 color_patern5 [2]=clear_led;
-	 color_patern5 [3]= red;
-	 color_patern5 [4]=clear_led;
-	 color_patern5 [5]= red;
-	 color_patern5 [6]=clear_led;
-
+		led_patern_setup();
 
    //led_test();
 	 led_patern1 (30) ;
@@ -126,7 +199,8 @@ void setup() {
   //RGB.control(true);
 
    // connect to the server
-   client.connect("ModbusMaster", "angel","xfilesxvf");
+  // client.connect("ModbusMaster", "angel","xfilesxvf");  //old credentials
+   client.connect("ModbusMaster", "evert","evert_1234");
       // publish/subscribe
    if (client.isConnected()) {
        // it can use messageid parameter at 4.
@@ -135,6 +209,7 @@ void setup() {
        client.publish("outTopic/message", buf);// "hello world QOS1", MQTT::QOS1, &messageid);
        client.subscribe("inTopic/message");
    }
+	  publish_data();
 }
 
 void led_patern1 (uint16_t blk_time) {
@@ -153,13 +228,81 @@ for (m = 0; m < 9; m++) {
 
 
 
+
 void loop() {
     if (client.isConnected())
         client.loop();
 
 unsigned long currentMillis = millis();
 if (currentMillis - previousMillis >= POLLING_TIME) {
-	publish_data();
+	//Spark.publish("tripped or not ",tripped);
+
+
+ if ((regs_data[17] & (1<<0))? 1:0)
+//if (((regs_data[17] >> 0)&1) & (tripped=0))
+    {   //test StatusWord bit 0 to know if drive is healthy or no  (1<<bitnumber)
+     Spark.publish("DRIVEHEALTH","VFD IS  OK");
+		// publish_data();
+		// regs_dataf[16]=regs_data[17];
+		// publish_data();
+		 publish_data();
+     tripped =0;
+    } else
+		{
+			 tripped =1;
+			 Spark.publish("DRIVEHEALTH","VFD IS  tripped");
+			 frozzen_data() ;
+			 regs_data[17]=regs_dataf[16];
+             temp_hum ();   // call either enviromental data
+		}
+/*
+		if ((regs_dataf[16] & (1<<0))? 0:1)
+	 //if (((regs_data[17] >> 0)&1) & (tripped=0))
+	     {   //test StatusWord bit 0 to know if drive is healthy or no  (1<<bitnumber)
+	      Spark.publish("DRIVEHEALTH","VFD IS  Tripped");
+	 		// publish_data();
+	 		 regs_data[17]=regs_dataf[16];
+			// frozzen_data() ;
+	      tripped =1;
+	     }
+*/
+/*
+//else
+  //  {
+	//   Spark.publish("DRIVEHEALTH","VFD entered in a tripped staate");
+	   //frozzen_data() ;
+	//regs_data[17]= regs_dataf[16];
+	//   tripped = 1;
+//    }
+if (regs_dataf[16] & (1<<0) & (tripped=1))
+    {  //if drive trip have been reset return to send the normal data stream
+	//	Spark.publish("DRIVE HAVE BEEN RESET");
+	//	publish_data();
+		regs_data[17]=regs_dataf[16];
+		Spark.publish("DRIVE HAVE BEEN RESET");
+		tripped =0;
+	  }
+	// else
+	//  {
+	//	 tripped =1;
+		 //Spark.publish("DRIVEHEALTH","VFD entered in a tripped staate");
+	//  }
+*/
+/*
+		if (tripped=1)
+		{
+	// publish_data();
+	// regs_dataf[16]=regs_data[17];
+	   frozzen_data() ;
+		 regs_data[17]=regs_dataf[16];
+		 }
+		 if  (tripped=0) {
+			 publish_data();
+       regs_dataf[16]= regs_data[17];
+		 }
+*/
+//bool drive_healthy=
+//}
 previousMillis = currentMillis;
 Serial.println("");
 Serial.println("Waiting for next cycle..");
@@ -176,7 +319,7 @@ Serial.println("");
 for (k = 0; k < 24; k++) {
  // getHoldingRegisterData( regs[k],  1,  data);
 if (getHoldingRegisterData(  regs_CSK[k],  1,  data)) {//symetrical current limit in % 0.1 dec place
-delay(20);
+delay(10);
 	 regs_data[k]=data[0];
 	 Serial.printf("Register data : %d: ",  regs_CSK[k]);
 	 Serial.print(regs_data[k], DEC);
@@ -306,6 +449,56 @@ return false;
 }
 
 
+void frozzen_data() {
+uint8_t u;
+Serial.println(F("Starting Reading registers..."));
+Serial.println("");
+for (u = 0; u < 18; u++) {
+ // getHoldingRegisterData( regs[k],  1,  data);
+if (getHoldingRegisterData(frozen_regs[u] ,  1,  dataf)) {//symetrical current limit in % 0.1 dec place
+delay(10);
+	 regs_dataf[u]=dataf[0];
+	 Serial.printf("Register data : %d: ",  frozen_regs[u]);
+	 Serial.print(regs_dataf[u], DEC);
+
+	 Serial.println(F(" "));
+ }
+}
+
+sprintf(vfd_frzndata1,"{\"VFD_ID\":%d, \"Ref_sel\":%.2f, \"PreSk_fref\":%.2f, \"PreR_ref\":%.2f, \"Ref_rpm\":%d, \"Clp_ref\":%.2f, \"PostR_Ref\":%.2f, \"Finl_Dref\":%.2f, \"Ana1\":%.2f,\"Ana2\":%.2f}",DRIVEID, regs_dataf[0]/100.00, regs_dataf[1]/100.0, regs_dataf[2]/100.0, regs_dataf[3], regs_dataf[4]/100.0,regs_dataf[5]/100.0,regs_dataf[6]/100.0,regs_dataf[14]/100.0,regs_dataf[15]/100.0);
+sprintf(vfd_frzndata2,"{\"VFD_ID\":%d, \"Curr_mag\":%.2f, \"Torq_pcurr\":%.2f, \"Magn_curr\":%.2f, \"Out_frec\":%.2f, \"Out_volt\":%d,\"Out_pwr\":%.2f, \"Dcb_volt\":%d ,\"Stt_wrd\":%d,\"Stack_temp\":%d}",DRIVEID, regs_dataf[7]/100.0, regs_dataf[8]/100.0,regs_dataf[9]/100.0,regs_dataf[10]/100.0,regs_dataf[11],regs_dataf[12]/100.0,regs_dataf[13],regs_dataf[16],regs_dataf[17]);
+Spark.publish("VFD_TRIP_FROZEN_DATA1",vfd_frzndata1);
+Spark.publish("VFD_TRIP_FROZEN_DATA2",vfd_frzndata2);
+
+
+
+//client.publish("VFDDATA",vfd_data);
+Serial.println("");
+//if(DEBUGON){
+Serial.println(vfd_frzndata1);
+Serial.println("");
+//}
+ if (client.publish("VFD_TRIP_FROZEN_DATA1",vfd_frzndata1) & client.publish("VFD_TRIP_FROZEN_DATA2",vfd_frzndata2) ) {  //Mqtt publish data  (node-red)
+	 white_led();
+	 Serial.println("Publishing data to Mqtt");
+  } else {
+		    red_led();
+		Serial.println("Mqtt Failed ..check Mqtt Broker");
+ }
+ Serial.println("");
+
+//temp_hum ();
+
+}
+
+
+
+
+
+
+
+
+
 void callback(char* topic, byte* payload, unsigned int length) {
     char p[length + 1];
     memcpy(p, payload, length);
@@ -314,7 +507,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
 		    data_sent_ok_mqtt();  //blink a led
     else if (!strcmp(p, "DEBUGOFF"))
         led_patern1 (50);    // blink  a led
-    delay(100);
+    delay(10);
 }
 
 // if application use QOS1 or QOS2, MQTT server sendback ack message id.
@@ -423,6 +616,23 @@ void red_led(){
   strip.setPixelColor(0, clear_led); strip.show(); // set LED 10 to the color in variable c (red)
 }
 
+void green_led(){
+  strip.setPixelColor(0, clear_led); strip.show(); // set LED 10 to the color in variable c (red)
+  strip.setPixelColor(1, clear_led); strip.show(); // set LED 10 to the color in variable c (red)
+
+  strip.setPixelColor(0, green); strip.show();
+  delay (60);
+  strip.setPixelColor(0, clear_led); strip.show(); // set LED 10 to the color in variable c (red)
+  delay(50);
+   strip.setPixelColor(0, green); strip.show();
+  delay (60);
+  strip.setPixelColor(0, clear_led); strip.show(); // set LED 10 to the color in variable c (red)
+  delay(50);
+  strip.setPixelColor(0, green); strip.show();
+  delay (60);
+  strip.setPixelColor(0, clear_led); strip.show(); // set LED 10 to the color in variable c (red)
+}
+
 void temp_hum (){
 	float h = dht.getHumidity();
 	float f = dht.getTempFarenheit();
@@ -459,4 +669,60 @@ void temp_hum (){
  Serial.println(buf);
  client.publish("ENVIROMENT-DATA", buf);// "hello world QOS1", MQTT::QOS1, &messageid);
  Spark.publish("ENVIROMENT-DATA", buf);
+}
+
+void led_patern_setup(){
+	strip.begin();                         // Initialize strip
+	strip.show();        // (R,  G,  B)    // Update all LEDs (= turn OFF, since none of them have been set yet!)
+	red       = strip.Color(255, 0, 0);    // Red
+	green     = strip.Color(0, 255, 0);    // Green
+	blue      = strip.Color(0, 0, 255);    // Blue
+	white     = strip.Color(243, 63, 193); // White  was (0,255,100) \\\violeta
+	clear_led = strip.Color(0, 0, 0);      // clear
+	//patern1 test all colors
+	color_patern1 [0]=clear_led;
+	color_patern1 [1]=red;
+	color_patern1 [2]=clear_led;
+	color_patern1 [3]=green;
+	color_patern1 [4]=clear_led;
+	color_patern1 [5]=blue;
+	color_patern1 [6]=clear_led;
+	color_patern1 [7]=white;
+	color_patern1 [8]=clear_led;
+
+ //patern2 color white
+	color_patern2 [0]=clear_led;
+	color_patern2 [1]= white;
+	color_patern2 [2]=clear_led;
+	color_patern2 [3]= white;
+	color_patern2 [4]=clear_led;
+	color_patern2 [5]= white;
+	color_patern2 [6]=clear_led;
+ //patern3 color green
+ color_patern3 [0]=clear_led;
+ color_patern3 [1]= green;
+ color_patern3 [2]=clear_led;
+ color_patern3 [3]= green;
+ color_patern3 [4]=clear_led;
+ color_patern3 [5]= green;
+ color_patern3 [6]=clear_led;
+ //patern4 color blue
+ color_patern4 [0]=clear_led;
+ color_patern4 [1]= blue;
+ color_patern4 [2]=clear_led;
+ color_patern4 [3]= blue;
+ color_patern4 [4]=clear_led;
+ color_patern4 [5]= blue;
+ color_patern4 [6]=clear_led;
+ //patern5 color red
+ color_patern5 [0]=clear_led;
+ color_patern5 [1]= red;
+ color_patern5 [2]=clear_led;
+ color_patern5 [3]= red;
+ color_patern5 [4]=clear_led;
+ color_patern5 [5]= red;
+ color_patern5 [6]=clear_led;
+
+
+
 }
